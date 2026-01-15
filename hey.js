057e57564eb6469bec2a0b83c587007ad76013a2
Anonni Mousse <61146153+maridasi47500@users.noreply.github.com>
2026-01-15 10:34:13 -0300

    dwf

--- hey.js
+++ hey.js
@@ -3,9 +3,29 @@ const app = express()
 const port = 3000
 var ejs = require('ejs');
 app.set('view engine', 'ejs');
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'dbuser',
+  password: 's3kreee7',
+  database: 'my_db'
+})
+
+
 
 
 app.get('/', (req, res) => {
+
+  connection.connect()
+  
+  connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    if (err) throw err
+  
+    console.log('The solution is: ', rows[0].solution)
+  })
+  
+  connection.end()
+
   res.render('job.ejs');
   //res.render('job.ejs', {}, function(err, data) {
   ////console.log(err || data)
